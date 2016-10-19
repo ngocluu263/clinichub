40923856bf22ef803797816fd3d581538cb5294a
Krittin Meenrattanakorn <Whatevermarch@gmail.com>
2016-10-19 23:25:21 +0700

    Apply embedded document and reference
    - At User model, is User still to be used? 
    - At Clinic model, I think it should stock the doctors name for quick fetching when creating session.
    - Please recheck the correctness of each model, especially the model that has List, Embedded Document, Reference.

--- clinichub/models.py
+++ clinichub/models.py
@@ -1,39 +1,58 @@
 from django.db import models
 from mongoengine import *
 
-
+# Is User still to be used? 
 class User(Document):
     username = StringField(required=True, max_length=20)
     firstname = StringField(required=True,max_length=50)
     lastname = StringField(required=True,max_length=50)
 
+class Patient(Document):
+	username = StringField(required=True, max_length=20)
+	password = StringField(required=True, max_length=50)
+	firstname = StringField(required=True,max_length=50)
+	lastname = StringField(required=True,max_length=50)
+	email = StringField(required=True,max_length=50)
+	balance = DecimalField()
+	
+class Doctor(Document):
+	username = StringField(required=True, max_length=20)
+	password = StringField(required=True, max_length=50)
+	firstname = StringField(required=True,max_length=50)
+	lastname = StringField(required=True,max_length=50)
+	email = StringField(required=True,max_length=50)
+	field = StringField(max_length=50)
+	clinic = ReferenceField('Clinic')
+
 class Clinic(Document):
 	clinic_name = StringField(required = True, max_length=50)
-	clinic_descliption = StringField(max_length=100)
+	clinic_description = StringField(max_length=100)
+	clinic_contrib = ListField(ReferenceField(Message))
 
+class Message(EmbeddedDocument):
+	msg = StringField(max_length=100)
+	sender = StringField(required=True,max_length=1)
+	timestamp = DateTimeField()
+	
 class Session(Document):
 	topic = StringField(max_length=50)
-	ses_patient = IntField()
-	ses_doctor = IntField()
+	ses_patient = ReferenceField(Patient)
+	ses_doctor = ReferenceField(Doctor)
+	message = ListField(EmbeddedDocumentField(Message))
 
 class Appointments(Document):
-	app_doctor = IntField()
-	app_patient = IntField()
+	app_doctor = ReferenceField(Doctor)
+	app_patient = ReferenceField(Patient)
 	app_time = DateTimeField()
 	app_note = StringField(max_length=100)
 
-class Transcript(Document):
-	trans_doctor = IntField()
-	trans_patient = IntField()
-	trans_note = StringField(max_length = 100)
-
-class Message(Document):
-	msg = StringField(max_length=100)
-	sender = StringField(max_length=1)
-
-class Drug(Document):
+class Drug(EmbeddedDocument):
 	drug_name = StringField(max_length=50)
 	drug_amount = IntField()
 	drug_usage = StringField(max_length=100)
-
-
+	
+class Transcript(Document):
+	trans_doctor = ReferenceField(Doctor)
+	trans_patient = ReferenceField(Patient)
+	trans_drugs = ListField(EmbeddedDocumentField(Drug))
+	trans_note = StringField(max_length = 100)
