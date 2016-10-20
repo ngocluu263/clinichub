f8f8ff20437763e696f73fd28e45d474be16366f
Jenwich Rattanayenjai <pamjeavrat@gmail.com>
2016-10-20 15:03:19 +0700

    Split views & Fix index view to not create a user

--- clinichub/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from .models import User
-
-def index(request):
-    user = User.objects.create(
-        username = "somebody"
-    )
-    user.save()
-    return render(request, 'index.html')
