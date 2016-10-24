c0e4d8293fdd3ffb59c11a2baeaffbc7135d7552
Jenwich Rattanayenjai <pamjeavrat@gmail.com>
2016-10-24 21:32:26 +0700

    Update redirecting when not logged in
    Redirect user to login page if user try to enter patient profile pages
    but hasn't logged in

--- clinichub/views/patient.py
+++ clinichub/views/patient.py
@@ -1,40 +1,64 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.core.urlresolvers import reverse
 from clinichub.models import *
 
 def patient_profile(request):
-    return render(request, 'patient/profile.html', {
-        'page': 'profile',
-        'username': request.session.get('username')
-    })
+    username = request.session.get('username')
+    if username:
+        return render(request, 'patient/profile.html', {
+            'page': 'profile',
+            'username': username
+        })
+    else:
+        return redirect(reverse('login'))
 
 def patient_sessions(request):
-    return render(request, 'patient/sessions.html', {
-        'page': 'sessions',
-        'username': request.session.get('username')
-    })
+    username = request.session.get('username')
+    if username:
+        return render(request, 'patient/sessions.html', {
+            'page': 'sessions',
+            'username': request.session.get('username')
+        })
+    else:
+        return redirect(reverse('login'))
 
 def patient_info(request):
-    return render(request, 'patient/info.html', {
-        'page': 'info',
-        'username': request.session.get('username')
-    })
+    username = request.session.get('username')
+    if username:
+        return render(request, 'patient/info.html', {
+            'page': 'info',
+            'username': request.session.get('username')
+        })
+    else:
+        return redirect(reverse('login'))
 
 def patient_payment(request):
-    return render(request, 'patient/payment.html', {
-        'page': 'payment',
-        'username': request.session.get('username')
-    })
+    username = request.session.get('username')
+    if username:
+        return render(request, 'patient/payment.html', {
+            'page': 'payment',
+            'username': request.session.get('username')
+        })
+    else:
+        return redirect(reverse('login'))
 
 def patient_appointments(request):
-    return render(request, 'patient/appointments.html', {
-        'page': 'appointments',
-        'username': request.session.get('username')
-    })
+    username = request.session.get('username')
+    if username:
+        return render(request, 'patient/appointments.html', {
+            'page': 'appointments',
+            'username': request.session.get('username')
+        })
+    else:
+        return redirect(reverse('login'))
 
 def patient_transcripts(request):
-    return render(request, 'patient/transcripts.html', {
-        'page': 'transcripts',
-        'username': request.session.get('username')
-    })
+    username = request.session.get('username')
+    if username:
+        return render(request, 'patient/transcripts.html', {
+            'page': 'transcripts',
+            'username': request.session.get('username')
+        })
+    else:
+        return redirect(reverse('login'))
