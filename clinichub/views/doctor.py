04a89e165dad295502044e5991d89710192da91b
Jenwich Rattanayenjai <pamjeavrat@gmail.com>
2016-10-24 23:37:07 +0700

    Update redirecting of patient and doctor profile pages
    When patient enter doctor page, it will redirect to patient page.
    When doctor enter patient page, it will redirect to doctor page.

--- clinichub/views/doctor.py
+++ clinichub/views/doctor.py
@@ -5,40 +5,55 @@ from clinichub.models import *
 
 def doctor_profile(request):
     if 'username' in request.session:
-        return render(request, 'doctor/profile.html', {
-            'page': 'profile',
-        })
+        if request.session.get('user_type') != 'doctor':
+            return redirect(reverse('patient_profile'))
+        else:
+            return render(request, 'doctor/profile.html', {
+                'page': 'profile',
+            })
     else:
         return redirect(reverse('doctor_login'))
 
 def doctor_sessions(request):
     if 'username' in request.session:
-        return render(request, 'doctor/sessions.html', {
-            'page': 'sessions',
-        })
+        if request.session.get('user_type') != 'doctor':
+            return redirect(reverse('patient_profile'))
+        else:
+            return render(request, 'doctor/sessions.html', {
+                'page': 'sessions',
+            })
     else:
         return redirect(reverse('doctor_login'))
 
 def doctor_info(request):
     if 'username' in request.session:
-        return render(request, 'doctor/info.html', {
-            'page': 'info',
-        })
+        if request.session.get('user_type') != 'doctor':
+            return redirect(reverse('patient_profile'))
+        else:
+            return render(request, 'doctor/info.html', {
+                'page': 'info',
+            })
     else:
         return redirect(reverse('doctor_login'))
 
 def doctor_clinic(request):
     if 'username' in request.session:
-        return render(request, 'doctor/clinic.html', {
-            'page': 'clinic',
-        })
+        if request.session.get('user_type') != 'doctor':
+            return redirect(reverse('patient_profile'))
+        else:
+            return render(request, 'doctor/clinic.html', {
+                'page': 'clinic',
+            })
     else:
         return redirect(reverse('doctor_login'))
 
 def doctor_appointments(request):
     if 'username' in request.session:
-        return render(request, 'doctor/appointments.html', {
-            'page': 'appointments',
-        })
+        if request.session.get('user_type') != 'doctor':
+            return redirect(reverse('patient_profile'))
+        else:
+            return render(request, 'doctor/appointments.html', {
+                'page': 'appointments',
+            })
     else:
         return redirect(reverse('doctor_login'))
--- clinichub/views/patient.py
+++ clinichub/views/patient.py
@@ -5,48 +5,66 @@ from clinichub.models import *
 
 def patient_profile(request):
     if 'username' in request.session:
-        return render(request, 'patient/profile.html', {
-            'page': 'profile',
-        })
+        if request.session.get('user_type') != 'patient':
+            return redirect(reverse('doctor_profile'))
+        else:
+            return render(request, 'patient/profile.html', {
+                'page': 'profile',
+            })
     else:
         return redirect(reverse('login'))
 
 def patient_sessions(request):
     if 'username' in request.session:
-        return render(request, 'patient/sessions.html', {
-            'page': 'sessions',
-        })
+        if request.session.get('user_type') != 'patient':
+            return redirect(reverse('doctor_profile'))
+        else:
+            return render(request, 'patient/sessions.html', {
+                'page': 'sessions',
+            })
     else:
         return redirect(reverse('login'))
 
 def patient_info(request):
     if 'username' in request.session:
-        return render(request, 'patient/info.html', {
-            'page': 'info',
-        })
+        if request.session.get('user_type') != 'patient':
+            return redirect(reverse('doctor_profile'))
+        else:
+            return render(request, 'patient/info.html', {
+                'page': 'info',
+            })
     else:
         return redirect(reverse('login'))
 
 def patient_payment(request):
     if 'username' in request.session:
-        return render(request, 'patient/payment.html', {
-            'page': 'payment',
-        })
+        if request.session.get('user_type') != 'patient':
+            return redirect(reverse('doctor_profile'))
+        else:
+            return render(request, 'patient/payment.html', {
+                'page': 'payment',
+            })
     else:
         return redirect(reverse('login'))
 
 def patient_appointments(request):
     if 'username' in request.session:
-        return render(request, 'patient/appointments.html', {
-            'page': 'appointments',
-        })
+        if request.session.get('user_type') != 'patient':
+            return redirect(reverse('doctor_profile'))
+        else:
+            return render(request, 'patient/appointments.html', {
+                'page': 'appointments',
+            })
     else:
         return redirect(reverse('login'))
 
 def patient_transcripts(request):
     if 'username' in request.session:
-        return render(request, 'patient/transcripts.html', {
-            'page': 'transcripts',
-        })
+        if request.session.get('user_type') != 'patient':
+            return redirect(reverse('doctor_profile'))
+        else:
+            return render(request, 'patient/transcripts.html', {
+                'page': 'transcripts',
+            })
     else:
         return redirect(reverse('login'))
