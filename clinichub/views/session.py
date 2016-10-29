410a298adb0aaa64dd57f659bd97c3a6ce5b9ca1
Jenwich Rattanayenjai <pamjeavrat@gmail.com>
2016-10-30 05:14:00 +0700

    Update handling invalid session url

--- clinichub/views/session.py
+++ clinichub/views/session.py
@@ -5,18 +5,23 @@ from clinichub.models import *
 
 def session(request, session_id):
     if 'username' in request.session:
-        session = Session.objects(id=session_id).first()
-        username = request.session.get('username')
-        user_type = request.session.get('user_type')
-        if user_type == 'patient':
-            user = Patient.objects(username=username).first()
-        else:
-            user = Doctor.objects(username=username).first()
-        if user == session.patient or user == session.doctor:
-            return render(request, 'session/session.html')
-        else:
+        try:
+            session = Session.objects(id=session_id).first()
+            if not session:
+                raise Exception('Session not found.')
+            username = request.session.get('username')
+            user_type = request.session.get('user_type')
+            if user_type == 'patient':
+                user = Patient.objects(username=username).first()
+            else:
+                user = Doctor.objects(username=username).first()
+            if user == session.patient or user == session.doctor:
+                return render(request, 'session/session.html')
+            else:
+                raise Exception('You do not have a permission to view this session.')
+        except Exception as e:
             return render(request, 'session/session.html', {
-                'error_message': 'You do not have a permission to view this session.'     
+                'error_message': e.args[0]   
             })
     else:
         return redirect(reverse('index'))
